8427799cca227d89922e69ea11e11eb331f788f4
tibuch <tim-oliver.buchholz@uni-konstanz.de>
2017-04-01 17:14:40 +0200

    Add Regularizer Interface.

---
+++ src/main/java/net/imagej/ops/fopd/regularizer/Regularizer.java
@@ -0,0 +1,11 @@
+package net.imagej.ops.fopd.regularizer;
+
+/**
+ * A {@link Regularizer} enforces a smoothness constraint on the solution.
+ * 
+ * @author Tim-Oliver Buchholz, University of Konstanz
+ *
+ */
+public interface Regularizer {
+	// NB: Marker Interface
+}
