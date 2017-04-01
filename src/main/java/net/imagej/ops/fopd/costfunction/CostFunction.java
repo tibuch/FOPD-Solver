a2fbe5980b12e2f1060d79fa954e2d8fb76dbbbb
tibuch <tim-oliver.buchholz@uni-konstanz.de>
2017-04-01 17:00:26 +0200

    Add CostFunction Interface.

---
+++ src/main/java/net/imagej/ops/fopd/costfunction/CostFunction.java
@@ -0,0 +1,12 @@
+package net.imagej.ops.fopd.costfunction;
+
+/**
+ * A {@link CostFunction} describes the difference between the current solution
+ * and the given observation.
+ * 
+ * @author Tim-Oliver Buchholz, University of Konstanz
+ *
+ */
+public interface CostFunction {
+	// NB: Marker Interface
+}
