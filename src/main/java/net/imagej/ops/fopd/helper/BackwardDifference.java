82bd3df8d33e1f4e43b4232ff470f793fabbcef7
tibuch <tim-oliver.buchholz@uni-konstanz.de>
2017-04-01 16:58:46 +0200

    Add BackwardDifference.

---
+++ src/main/java/net/imagej/ops/fopd/helper/BackwardDifference.java
@@ -0,0 +1,22 @@
+package net.imagej.ops.fopd.helper;
+
+import net.imagej.ops.special.hybrid.UnaryHybridCF;
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.type.Type;
+
+/**
+ * Backward Difference Interface.
+ * 
+ * Backward difference is defined by: bd(i(x)) = i(x) - i(x-1)
+ * 
+ * Ref: Weisstein, Eric W. "Backward Difference." From MathWorld--A Wolfram Web
+ * Resource. http://mathworld.wolfram.com/BackwardDifference.html
+ * 
+ * @author Tim-Oliver Buchholz, University of Konstanz
+ *
+ * @param <T>
+ */
+public interface BackwardDifference<T extends Type<T>>
+		extends UnaryHybridCF<RandomAccessibleInterval<T>, RandomAccessibleInterval<T>> {
+	// NB: Marker Interface
+}
---
+++ src/test/java/net/imagej/ops/fopd/helper/BackwardDifferenceTest.java
@@ -0,0 +1,45 @@
+package net.imagej.ops.fopd.helper;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import net.imagej.ops.fopd.AbstractOpTest;
+import net.imglib2.Cursor;
+import net.imglib2.img.Img;
+import net.imglib2.type.numeric.real.DoubleType;
+
+/**
+ * Test for {@link DefaultBackwardDifference}.
+ * 
+ * @author Tim-Oliver Buchholz, University of Konstanz
+ *
+ */
+public class BackwardDifferenceTest extends AbstractOpTest {
+
+	private static double[] backwardDifferenceX = new double[] { 0.0, -1.0, 1.0, 0.0, 0.0, 0.0, 0.0, -1.0, 1.0 };
+
+	private static double[] backwardDifferenceY = new double[] { 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, -1.0, 0.0 };
+
+	@Test
+	public void backwardDifferenceXTest() {
+
+		@SuppressWarnings("unchecked")
+		final Cursor<DoubleType> c = ((Img<DoubleType>) ops.run(DefaultBackwardDifference.class, img, 0)).cursor();
+		int i = 0;
+		while (c.hasNext()) {
+			assertEquals("GradientX differs", backwardDifferenceX[i++], c.next().get(), 0);
+		}
+	}
+
+	@Test
+	public void backwardDifferenceYTest() {
+
+		@SuppressWarnings("unchecked")
+		final Cursor<DoubleType> c = ((Img<DoubleType>) ops.run(DefaultBackwardDifference.class, img, 1)).cursor();
+		int i = 0;
+		while (c.hasNext()) {
+			assertEquals("GradientY differs", backwardDifferenceY[i++], c.next().get(), 0);
+		}
+	}
+}
