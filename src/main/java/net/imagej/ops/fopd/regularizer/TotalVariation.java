fcefd062197137ac0ce5462b2e5bede6e43218e0
tibuch <tim-oliver.buchholz@uni-konstanz.de>
2017-04-01 17:14:51 +0200

    Add Total Variation Interface.

---
+++ src/main/java/net/imagej/ops/fopd/regularizer/TotalVariation.java
@@ -0,0 +1,15 @@
+package net.imagej.ops.fopd.regularizer;
+
+/**
+ * {@link TotalVariation} implements the Total Variation Regularizer.
+ * 
+ * Ref: Rowland, Todd. "Total Variation." From MathWorld--A Wolfram Web
+ * Resource, created by Eric W. Weisstein.
+ * http://mathworld.wolfram.com/TotalVariation.html
+ * 
+ * @author Tim-Oliver Buchholz, University of Konstanz
+ *
+ */
+public interface TotalVariation extends Regularizer {
+	// NB: Marker Interface
+}
