5e41ed1e078db894165c3a2928ed15e5dd54847b
tibuch <tim-oliver.buchholz@uni-konstanz.de>
2017-04-01 17:00:37 +0200

    Add Denoising Interface.

---
+++ src/main/java/net/imagej/ops/fopd/costfunction/Denoising.java
@@ -0,0 +1,12 @@
+package net.imagej.ops.fopd.costfunction;
+
+/**
+ * A {@link Denoising} implements a {@link CostFunction} which solves denoising
+ * problems.
+ * 
+ * @author Tim-Oliver Buchholz, University of Konstanz
+ *
+ */
+public interface Denoising extends CostFunction {
+	// NB: Marker Interface
+}
