bded74c59d502957cc7bccce8f0c6c924d133b5c
tibuch <tim-oliver.buchholz@uni-konstanz.de>
2017-04-03 17:35:50 +0200

    Refactor L1-Denoising of one 2D image.

---
+++ src/main/java/net/imagej/ops/fopd/costfunction/AbstractCostFunction.java
@@ -0,0 +1,27 @@
+package net.imagej.ops.fopd.costfunction;
+
+import net.imagej.ops.fopd.Ascent;
+import net.imagej.ops.fopd.Descent;
+import net.imglib2.type.numeric.RealType;
+
+/**
+ * Abstract implementation of {@link CostFunction}.
+ * 
+ * @author Tim-Oliver Buchholz, University of Konstanz
+ *
+ * @param <T>
+ */
+public class AbstractCostFunction<T extends RealType<T>> implements CostFunction<T> {
+
+	protected Ascent<T> ascent;
+
+	protected Descent<T> descent;
+
+	public Ascent<T> getAscent() {
+		return this.ascent;
+	}
+
+	public Descent<T> getDescent() {
+		return this.descent;
+	}
+}
--- src/main/java/net/imagej/ops/fopd/costfunction/CostFunction.java
+++ src/main/java/net/imagej/ops/fopd/costfunction/CostFunction.java
@@ -1,12 +1,20 @@
 package net.imagej.ops.fopd.costfunction;
 
+import net.imagej.ops.fopd.Ascent;
+import net.imagej.ops.fopd.Descent;
+import net.imglib2.type.numeric.RealType;
+
 /**
- * A {@link CostFunction} describes the difference between the current solution
- * and the given observation.
+ * A {@link CostFunction} consists of an {@link Ascent}-step and a
+ * {@link Descent}-step. In some sense the cost function describeds the image
+ * formation process.
  * 
  * @author Tim-Oliver Buchholz, University of Konstanz
  *
  */
-public interface CostFunction {
-	// NB: Marker Interface
+public interface CostFunction<T extends RealType<T>> {
+
+	public Ascent<T> getAscent();
+
+	public Descent<T> getDescent();
 }
