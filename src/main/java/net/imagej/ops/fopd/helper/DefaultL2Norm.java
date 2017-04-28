0bf26b0f8ccfee88fc64fc5b32eb20a97d0d9eec
tibuch <tim-oliver.buchholz@uni-konstanz.de>
2017-04-28 19:03:30 +0200

    Add 9D implementation of L2-Norm.

--- src/main/java/net/imagej/ops/fopd/helper/DefaultL2Norm.java
+++ src/main/java/net/imagej/ops/fopd/helper/DefaultL2Norm.java
@@ -64,7 +64,10 @@ public class DefaultL2Norm<T extends RealType<T>> extends
 		else if (numDualVariables == 4) {
 			norm4D(input[0], input[1], input[2], input[3], output);
 		}
-		else {
+		else if (numDualVariables == 9) {
+			norm9D(input[0], input[1], input[2], input[3], input[4], 
+					input[5], input[6], input[7], input[8], output);
+		} else {
 			normND(input, output);
 		}
 	}
@@ -377,4 +380,119 @@ public class DefaultL2Norm<T extends RealType<T>> extends
 			}
 		}
 	}
+	
+	private void norm9D(final RandomAccessible<T> source0,
+			final RandomAccessible<T> source1, final RandomAccessible<T> source2,
+			final RandomAccessible<T> source3, final RandomAccessible<T> source4,
+			final RandomAccessible<T> source5, final RandomAccessible<T> source6,
+			final RandomAccessible<T> source7, final RandomAccessible<T> source8,
+			final RandomAccessibleInterval<T> norm)
+		{
+			final int n = source0.numDimensions();
+
+			final long[] min = new long[n];
+			norm.min(min);
+			final long[] max = new long[n];
+			norm.max(max);
+			final long[] shiftback = new long[n];
+			for (int d = 0; d < n; ++d)
+				shiftback[d] = min[d] - max[d];
+
+			final RandomAccess<T> result = norm.randomAccess();
+			final RandomAccess<T> s1 = source0.randomAccess();
+			final RandomAccess<T> s2 = source1.randomAccess();
+			final RandomAccess<T> s3 = source2.randomAccess();
+			final RandomAccess<T> s4 = source3.randomAccess();
+			final RandomAccess<T> s5 = source4.randomAccess();
+			final RandomAccess<T> s6 = source5.randomAccess();
+			final RandomAccess<T> s7 = source6.randomAccess();
+			final RandomAccess<T> s8 = source7.randomAccess();
+			final RandomAccess<T> s9 = source8.randomAccess();
+
+			result.setPosition(min);
+			s1.setPosition(min);
+			s2.setPosition(min);
+			s3.setPosition(min);
+			s4.setPosition(min);
+			s5.setPosition(min);
+			s6.setPosition(min);
+			s7.setPosition(min);
+			s8.setPosition(min);
+			s9.setPosition(min);
+			
+			final long max0 = max[0];
+			while (true) {
+				// process pixel
+				final T t = result.get();
+				final double t1 = s1.get().getRealDouble();
+				final double t2 = s2.get().getRealDouble();
+				final double t3 = s3.get().getRealDouble();
+				final double t4 = s4.get().getRealDouble();
+				final double t5 = s5.get().getRealDouble();
+				final double t6 = s6.get().getRealDouble();
+				final double t7 = s7.get().getRealDouble();
+				final double t8 = s8.get().getRealDouble();
+				final double t9 = s9.get().getRealDouble();
+				t.setReal(
+					Math.sqrt(t1 * t1 + t2 * t2 + t3 * t3 + t4 * t4 + 
+							t5 * t5 + t6 * t6 + t7 * t7 + t8 * t8 + t9 * t9));
+
+				// move to next pixel
+				// check dimension 0 separately to avoid the loop over d in most
+				// iterations
+				if (result.getLongPosition(0) == max0) {
+					if (n == 1) return;
+					result.move(shiftback[0], 0);
+					s1.move(shiftback[0], 0);
+					s2.move(shiftback[0], 0);
+					s3.move(shiftback[0], 0);
+					s4.move(shiftback[0], 0);
+					s5.move(shiftback[0], 0);
+					s6.move(shiftback[0], 0);
+					s7.move(shiftback[0], 0);
+					s8.move(shiftback[0], 0);
+					s9.move(shiftback[0], 0);
+					// now check the remaining dimensions
+					for (int d = 1; d < n; ++d)
+						if (result.getLongPosition(d) == max[d]) {
+							result.move(shiftback[d], d);
+							s1.move(shiftback[d], d);
+							s2.move(shiftback[d], d);
+							s3.move(shiftback[d], d);
+							s4.move(shiftback[d], d);
+							s5.move(shiftback[d], d);
+							s6.move(shiftback[d], d);
+							s7.move(shiftback[d], d);
+							s8.move(shiftback[d], d);
+							s9.move(shiftback[d], d);
+							if (d == n - 1) return;
+						}
+						else {
+							result.fwd(d);
+							s1.fwd(d);
+							s2.fwd(d);
+							s3.fwd(d);
+							s4.fwd(d);
+							s5.fwd(d);
+							s6.fwd(d);
+							s7.fwd(d);
+							s8.fwd(d);
+							s9.fwd(d);
+							break;
+						}
+				}
+				else {
+					result.fwd(0);
+					s1.fwd(0);
+					s2.fwd(0);
+					s3.fwd(0);
+					s4.fwd(0);
+					s5.fwd(0);
+					s6.fwd(0);
+					s7.fwd(0);
+					s8.fwd(0);
+					s9.fwd(0);
+				}
+			}
+		}
 }
