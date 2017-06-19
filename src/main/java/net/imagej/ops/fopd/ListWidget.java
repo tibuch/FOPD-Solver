baa574cb22a6a67b66bfbe54ce6a71d41346571c
tibuch <tim-oliver.buchholz@uni-konstanz.de>
2017-06-19 21:55:37 +0200

    Add init ListWidget.

---
+++ src/main/java/net/imagej/ops/fopd/ListWidget.java
@@ -0,0 +1,14 @@
+package net.imagej.ops.fopd;
+
+import java.util.List;
+
+import org.scijava.Context;
+import org.scijava.plugin.PluginInfo;
+import org.scijava.ui.AbstractUIInputWidget;
+import org.scijava.widget.InputWidget;
+import org.scijava.widget.WidgetModel;
+
+public class ListWidget<T, W> extends AbstractUIInputWidget<List<T>, W> implements InputWidget<List<T>, W> {
+
+
+}
